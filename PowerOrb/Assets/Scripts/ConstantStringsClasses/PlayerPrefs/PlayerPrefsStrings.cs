742a33776db0e4b1cd665ca11b598ddd6e54f383
ominousapple <anastasov.r.peter@gmail.com>
2018-10-29 15:36:47 +0200

    Add Comment 3

--- PowerOrb/Assets/Scripts/ConstantStringsClasses/PlayerPrefs/PlayerPrefsStrings.cs
+++ PowerOrb/Assets/Scripts/ConstantStringsClasses/PlayerPrefs/PlayerPrefsStrings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 // comment 1
+// comment 3
 public class PlayerPrefsStrings {
     #region Window Manager Prefs
     //Window Manager Uses this key to store/access saved resolution index 
