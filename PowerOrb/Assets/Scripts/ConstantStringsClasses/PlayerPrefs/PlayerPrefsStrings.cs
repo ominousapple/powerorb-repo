af25650a81a863e28e10802044ebf97f0899b88b
ominousapple <anastasov.r.peter@gmail.com>
2018-10-29 17:00:30 +0200

    Add "// Master changed 3" to master

--- PowerOrb/Assets/Scripts/ConstantStringsClasses/PlayerPrefs/PlayerPrefsStrings.cs
+++ PowerOrb/Assets/Scripts/ConstantStringsClasses/PlayerPrefs/PlayerPrefsStrings.cs
@@ -11,6 +11,8 @@ using System.Collections.Generic;
 
 // Master changed 2
 
+// Master changed 3
+
 public class PlayerPrefsStrings {
     #region Window Manager Prefs
     //Window Manager Uses this key to store/access saved resolution index 
