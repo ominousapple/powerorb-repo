7f99e28f5d120ea6f01dea83d332efdaa7fd4613
ominousapple <anastasov.r.peter@gmail.com>
2018-11-18 01:18:06 +0200

    Add/Fix Features, Main Menu Feature player movement and talking

---
+++ PowerOrb/Assets/Scripts/GameplayScene/Player/AIMenuPlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMenuPlayerInput : InputIControllable
+{
+    int horInp = -1;
+
+	// Use this for initialization
+	void Start () {
+        ChangedHorizontal(horInp);
+        //StartCoroutine(TalkEveryFiveSeconds());
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+       if (collision.tag == "MenuTurnAroundBox") {
+            
+            horInp = -horInp;
+
+            ChangedHorizontal(0);
+            StartCoroutine(TalkEverySevenSeconds());
+
+        }
+
+        
+    }
+
+    IEnumerator TalkEverySevenSeconds() {
+
+        GetComponent<Player>().TalkDialogue();
+        yield return new WaitForSeconds(7);
+        ChangedHorizontal(horInp);
+
+    }
+
+
+}
