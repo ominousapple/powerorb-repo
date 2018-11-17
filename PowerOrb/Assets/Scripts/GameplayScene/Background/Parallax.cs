0d0e4913e4c9281c60427186a43bd171e5f4fb1b
ominousapple <anastasov.r.peter@gmail.com>
2018-11-17 16:09:56 +0200

    Add Background Generation

--- PowerOrb/Assets/Scripts/GameplayScene/Background/Parallax.cs
+++ PowerOrb/Assets/Scripts/GameplayScene/Background/Parallax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Parallax : MonoBehaviour {
     #region Parallax Attributes:
     private GameObject[] BackgroundGameObjects;
@@ -13,14 +14,60 @@ public class Parallax : MonoBehaviour {
     private Vector3 previousCamPos;
     #endregion
 
+    #region GenerateBackgrounds Attrivutes:
+
+    private string GeneratedBGTag = "SetBG";
+    private bool isStarted = false;
+    [SerializeField]
+    private int numberBGLeft = 0;
+    [SerializeField]
+    private int numberBGRight = 0;
+
+    private int allGeneratedBGs = 0;
+
+    [SerializeField]
+    private float distanceBetweenBGSetsX = 0f;
+    [SerializeField]
+    private float distanceBetweenBGSetsY = 0f;
+
+    [SerializeField]
+    private GameObject BackgroundPrefab = null;
+
+    [SerializeField]
+    private GameObject DownwardsBackgroundPrefab = null;
+    
+
+    [SerializeField]
+    private GameObject ParentGameObjectOfBackgrounds = null;
+
+    //private GameObject[] AllGeneratedBGs;
+    private GameObject[] AllGeneratedBGsDELETE;
+
+    #endregion
+
     void Awake()
     {
+        //allGeneratedBGs = numberBGLeft + numberBGRight;
+        //AllGeneratedBGs = new GameObject[numberBGLeft + numberBGRight];
         cam = Camera.main.transform;
 
     }
     // Use this for initialization
     void Start () {
-        BackgroundGameObjects = GameObject.FindGameObjectsWithTag("Parallax");
+
+        //GeneratingBackgrounds:
+        AllGeneratedBGsDELETE = GameObject.FindGameObjectsWithTag(GeneratedBGTag);
+        if (!isStarted)
+        {
+            DestroySetBGs();
+            GenerateBackgrounds();
+
+            isStarted = true;
+        }
+
+
+            //Parallax
+            BackgroundGameObjects = GameObject.FindGameObjectsWithTag("Parallax");
 
         backgrounds = new Transform[BackgroundGameObjects.Length];
 
@@ -44,7 +91,14 @@ public class Parallax : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        for (int i = 0; i < backgrounds.Length; i++) {
+        if (!isStarted) {
+            DestroySetBGsUpdate();
+            GenerateBackgrounds();
+            isStarted = true;
+        }
+
+
+            for (int i = 0; i < backgrounds.Length; i++) {
             //The parallax is the opposite of the camera movement, because the previous frame multiplied by the scale
             //How much is moved, multiplied by the amount of parallaxScales[i]
             float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
@@ -67,4 +121,95 @@ public class Parallax : MonoBehaviour {
         previousCamPos = cam.position;
 
     }
+
+
+
+    void OnValidate()
+    {
+        isStarted = false;
+     
+    }
+
+    private void GenerateBackgrounds() {
+        if (ParentGameObjectOfBackgrounds != null && BackgroundPrefab != null && DownwardsBackgroundPrefab != null)
+        {
+
+            
+            
+
+
+            for (int i = 0; i < numberBGRight; i++) { 
+            
+            GameObject childBackgroundSet = Instantiate(BackgroundPrefab, ParentGameObjectOfBackgrounds.transform.position, Quaternion.identity);
+            childBackgroundSet.transform.parent = ParentGameObjectOfBackgrounds.transform;
+            childBackgroundSet.transform.position = new Vector2(distanceBetweenBGSetsX*(i+1), ParentGameObjectOfBackgrounds.transform.position.y);
+                childBackgroundSet.tag = GeneratedBGTag;
+
+                GameObject childBackgroundSet2 = Instantiate(DownwardsBackgroundPrefab, ParentGameObjectOfBackgrounds.transform.position, Quaternion.identity);
+                childBackgroundSet2.transform.parent = ParentGameObjectOfBackgrounds.transform;
+                childBackgroundSet2.transform.position = new Vector2(distanceBetweenBGSetsX * (i + 1), ParentGameObjectOfBackgrounds.transform.position.y + distanceBetweenBGSetsY);
+                childBackgroundSet2.tag = GeneratedBGTag;
+
+
+
+            }
+
+            for (int i = 0; i < numberBGLeft; i++)
+            {
+
+                GameObject childBackgroundSet = Instantiate(BackgroundPrefab, ParentGameObjectOfBackgrounds.transform.position, Quaternion.identity);
+                childBackgroundSet.transform.parent = ParentGameObjectOfBackgrounds.transform;
+                childBackgroundSet.transform.position = new Vector2(-distanceBetweenBGSetsX * (i + 1), ParentGameObjectOfBackgrounds.transform.position.y);
+                childBackgroundSet.tag = GeneratedBGTag;
+
+                    GameObject childBackgroundSet2 = Instantiate(DownwardsBackgroundPrefab, ParentGameObjectOfBackgrounds.transform.position, Quaternion.identity);
+                    childBackgroundSet2.transform.parent = ParentGameObjectOfBackgrounds.transform;
+                    childBackgroundSet2.transform.position = new Vector2(-distanceBetweenBGSetsX * (i + 1), ParentGameObjectOfBackgrounds.transform.position.y + distanceBetweenBGSetsY);
+                    childBackgroundSet2.tag = GeneratedBGTag;
+
+            }
+        }
+    }
+
+
+    private void DestroySetBGs()
+    {
+        if (AllGeneratedBGsDELETE != null)
+        {
+            for (int i = 0; i < AllGeneratedBGsDELETE.Length; i++)
+            {
+                if (AllGeneratedBGsDELETE[i] != null)
+                    DestroyImmediate(AllGeneratedBGsDELETE[i]);//DestroyImmediate
+            }
+        }
+
+    }
+
+    private void DestroySetBGsUpdate()
+    {
+        AllGeneratedBGsDELETE = null;
+        AllGeneratedBGsDELETE = GameObject.FindGameObjectsWithTag(GeneratedBGTag);
+        if (AllGeneratedBGsDELETE != null)
+        {
+            for (int i = 0; i < AllGeneratedBGsDELETE.Length; i++)
+            {
+                if (AllGeneratedBGsDELETE[i] != null)
+                    DestroyImmediate(AllGeneratedBGsDELETE[i]);//DestroyImmediate
+            }
+        }
+
+
+        //Restart Parallax algorithm attributes:
+        BackgroundGameObjects = null;
+        BackgroundGameObjects = GameObject.FindGameObjectsWithTag("Parallax");
+
+        backgrounds = new Transform[BackgroundGameObjects.Length];
+
+        for (int i = 0; i < BackgroundGameObjects.Length; i++)
+        {
+            backgrounds[i] = BackgroundGameObjects[i].GetComponent<Transform>();
+        }
+
+    }
+
 }
