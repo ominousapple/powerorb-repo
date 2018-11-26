e936db5f7ea9a92f0ae82ae96ee7cad3b1967fe8
ominousapple <anastasov.r.peter@gmail.com>
2018-11-27 01:02:22 +0200

    Add Finishing touches - Controls and Peter's level

---
+++ PowerOrb/Assets/Scripts/UtilityScripts/ShowControlsUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowControlsUI : MonoBehaviour {
+    public static ShowControlsUI instance = null;
+
+    [SerializeField]
+    private Image KeyboardControlsUI;
+    // Use this for initialization
+    void Awake () {
+        instance = this;
+    }
+    public void FlashControls()
+    {
+        
+        if (KeyboardControlsUI != null)
+        {
+            StartCoroutine(FlashControlsIEnumerator());
+        }
+    }
+
+     IEnumerator FlashControlsIEnumerator() {
+       
+            
+            KeyboardControlsUI.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        KeyboardControlsUI.gameObject.SetActive(false);
+        
+    }
+
+}
